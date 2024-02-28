b55ff34b60e0172d8f94390bb79b760213e20b97
Fenar Khan <fenarkhan@gmail.com>
2024-02-28 22:23:19 +0530

    jsdlkfjsdf

--- js/locomotive_gsap.js
+++ js/locomotive_gsap.js
@@ -1,37 +1,59 @@
-function loco(){
-    gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-
-// --- SETUP START ---
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+ScrollTrigger.defaults({
+  scroller: '#main',
+  markers: false
 });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.defaults({ scroller: "#main" });
-// --- SETUP END ---
+var scroll = new LocomotiveScroll( {
+    el: document.querySelector( '#main' ),
+    smooth: true,
+    multiplier: 1.0,
+    getDirection: true,
+});
+
+// Update scroll position
+scroll.on( 'scroll', ( instance ) => {
+    ScrollTrigger.update();
+    document.documentElement.setAttribute( 'data-scrolling', instance.direction );
+});
+
+// Scroll position for ScrollTrigger
+ScrollTrigger.scrollerProxy( '#main', {
+    scrollTop( value ) {
+        return arguments.length ? scroll.scrollTo( value, 0, 0 ) : scroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    pinType: document.querySelector( '#main' ).style.transform ? "transform" : "fixed"
+} );
 
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.addEventListener( 'refresh', () => scroll.update() );
 ScrollTrigger.refresh();
 
-}
 
-loco()
+gsap.to("#page>video" ,{
+    scrollTrigger:{
+        trigger: `#page>video`,
+        start: `3% top`,
+        end: `bottom top`,
+        markers: true,
+        scroller:`#main`
+    },
+    onStart:()=>{
+        document.querySelector("#page>video").play()
+        console.log('triggered')
+    }
+})
+
+gsap.to("#page",{
+    scrollTrigger:{
+        trigger: `#page`,
+        start: `top top`,
+        end: `bottom top`,
+        scroller:`#main`,
+        pin:true,
+    }
+})
